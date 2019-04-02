48c48a889773440c302d9ef82924ee571ff0aa9f
Rajat Pathak <rajatpathak786@gmail.com>
2019-04-02 16:36:53 +0530

    changes added in assignment 1

--- secondlargest.js
+++ secondlargest.js
@@ -1,35 +1,19 @@
-function secondLargest(array) {
-   let first = array[0] , second = array[1], dfirst = array[0], dsecond = array[1], largest, secondLargest, n = 0, i = 0;
-   while((array.length - 1) > n) {
-      if(first > second) {
-         largest = first;
-         second = array[n + 2];
-         ++n;        
-      } else  {
-          largest = second;
-          first = array[n + 2];
-          ++n;          
-      } 
-   } 
+function secondLargest(array) {    
+   let largest = -Infinity,secondLargest = -Infinity;
+   for(let i = 0; i < array.length; i++) {
+       if(array[i] > largest) {
+          largest = array[i];          
+       }
 
-   while ((array.length - 1) > i) {
-      if(array[i] == largest) { 
-         dfirst = (dfirst == largest? array[i+2]: dfirst);
-         dsecond = (dsecond == largest? array[i+2]: dsecond);
-         ++i;
-      } else if(dfirst > dsecond) {          
-          secondLargest = dfirst;
-          dsecond = array[i + 2]; 
-          ++i;       
-      } else {
-          secondLargest = dsecond;
-          dfirst = array[i + 2];
-          ++i;          
-      }
+   }
+   
+   for(let i = 0; i < array.length; i++) {
+       if((array[i] > secondLargest) && (array[i] != largest)) {
+           secondLargest = array[i];
+       }
 
-   } 
+   }
     
   return secondLargest;
+
 }
-
-
