3774db8ec224546dcbdefb185dfd7488ef670e6f
Rajat Pathak <rajatpathak786@gmail.com>
2019-04-02 18:13:37 +0530

    files modified good practices taken into consideration

--- flatten.js
+++ flatten.js
@@ -4,15 +4,12 @@ function flatten(unflatObject) {
     if (typeof unflatObject[i] == 'object' ) { 	               
       const flatObject = flatten(unflatObject[i]);    
       for (let x in flatObject) {
-	toReturn[i + '.' + x] = flatObject[x];                 
+	      toReturn[i + '.' + x] = flatObject[x];                 
       }
-
     } else {             
         toReturn[i] = unflatObject[i];  
-      }
-
-  }
-
-      return toReturn;
+    }
+  }  
+  return toReturn;
 }
 
--- secondlargest.js
+++ secondlargest.js
@@ -1,19 +1,14 @@
 function secondLargest(array) {    
-   let largest = -Infinity,secondLargest = -Infinity;
-   for(let i = 0; i < array.length; i++) {
-       if(array[i] > largest) {
-          largest = array[i];          
-       }
-
-   }
-   
-   for(let i = 0; i < array.length; i++) {
-       if(array[i] > secondLargest && array[i] != largest) {
-           secondLargest = array[i];
-       }
-
-   }
-    
+  let largest = -Infinity,secondLargest = -Infinity;
+  for(let i = 0; i < array.length; i++) {
+    if(array[i] > largest) {
+      largest = array[i];          
+    }
+  }   
+  for(let i = 0; i < array.length; i++) {
+    if(array[i] > secondLargest && array[i] != largest) {
+      secondLargest = array[i];
+    }
+  }    
   return secondLargest;
-
 }
