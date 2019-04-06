b50518e8f74add4adb0856effa5109927f1d4c50
Rajat Pathak <rajatpathak786@gmail.com>
2019-04-06 14:51:32 +0530

    callbackarray.js modified

--- callbackarray.js
+++ callbackarray.js
@@ -1,23 +1,21 @@
 const fs = require('fs');
 let arr = [], i = 0, l = 5, y = 0, x = 0;
 for ( i; i < l; i++) {
-  arr[i] = ('file'+(i+1)+'.txt');  
+  arr[i] = ('file'+(i + 1)+'.txt');  
 }
 sync(arr[0]);
-function sync(asd){ 
-  fs.readFile(asd,function (err,data) {
+function sync(asd) { 
+  fs.readFile(asd, function (err, data) {
     if (err) console.error('file location not found');
     else {
       console.log('=============');
-      console.log('=============');
       console.log(data.toString());
       console.log(data.toString());
-      if(y < l-1) {  
-      ++y;
-      sync(arr[y]);
+      if (y < l - 1) {  
+        ++y;
+        sync(arr[y]);
       } else {
-      console.log('=============');
-      console.log('=============');  
+        console.log('=============');  
       }
     }    
   });
