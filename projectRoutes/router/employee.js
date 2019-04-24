19c1546c6736879b6e8d9cfb30bfdd34ba239d47
Rajat Pathak <rajatpathak786@gmail.com>
2019-04-24 10:58:28 +0530

    files added

---
+++ projectRoutes/router/employee.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const employee = require('../../controller/employee');
+
+router.get('/employee', employee.employeeGet);
+router.post('/employee', employee.employeeInsert);
+router.get('/employeeFetch', employee.employeeFetch);
+router.get('/employeeFetchId', employee.employeeFetchId);
+router.post('/employeeUpdate', employee.employeeUpdate);
+router.get('/employeeDelete', employee.employeeDelete);
+
+module.exports={router};
